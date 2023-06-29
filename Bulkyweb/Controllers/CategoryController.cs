b93f1626a794ceb6b74ac8fdf460a612d05e7a75
Meghana Pawar <meghana011199@gmail.com>
2023-06-29 20:32:22 +0530

    Commit

---
+++ Bulkyweb/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkyweb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db)
+        {
+            _categoryRepo=db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //Custom Validation
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.TryAddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            //}
+				if(ModelState.IsValid)
+            {
+				_categoryRepo.Add(obj);
+				//_categoryRepo.save();
+				TempData["success"] = "Category created successfully";
+				return RedirectToAction("Index");
+			}
+                return View();
+		}
+
+		public IActionResult Edit(int? id)
+		{
+			if(id== null || id==0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+			if (categoryFromDb==null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			//Custom Validation
+			//if (obj.Name == obj.DisplayOrder.ToString())
+			//{
+			//    ModelState.TryAddModelError("name", "The DisplayOrder cannot exactly match the Name");
+			//}
+			if (ModelState.IsValid)
+			{
+				_categoryRepo.Update(obj);
+				//_categoryRepo.save();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _categoryRepo.Get(u => u.Id == id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_categoryRepo.Remove(obj);
+			//_categoryRepo.save();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+
+		}
+
+	}
+}
---
+++ Bulkyweb/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkyweb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepo;
+        public ProductController(IProductRepository db)
+        {
+            _productRepo = db;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _productRepo.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _productRepo.Add(obj);
+                TempData["success"] = "New Product is added successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _productRepo.Get(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _productRepo.Update(obj);
+
+                TempData["success"] = "Product updated";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _productRepo.Get(u => u.Id == id);
+
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _productRepo.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _productRepo.Remove(obj);
+
+            TempData["success"] = "Product deleted";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
